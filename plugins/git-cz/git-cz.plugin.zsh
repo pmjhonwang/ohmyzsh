e8cfba7731190cfc6e773d7fc258c1430b7c94be
pmjhonwang <pmjhonwang@gmail.com>
2025-10-19 14:01:57 +0800

    fix(git-cz): 🐛 update function

--- plugins/git-cz/git-cz.plugin.zsh
+++ plugins/git-cz/git-cz.plugin.zsh
@@ -2,5 +2,5 @@ function gczs {
   local all_args=("$@")
   local rest_args=("${all_args[@]:2}")
   echo "git cz $1 $2 ${rest_args[@]}"
-  git cz --scope=$1 --subject=$2 "${rest_args[@]}"
+  git cz --non-interactive --scope=$1 --subject=$2 "${rest_args[@]}"
 }
